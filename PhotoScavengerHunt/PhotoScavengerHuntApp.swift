85c15e2c425f771d3eba9571ea30437d3ae09975
Victoria Mckinnie <Vmcki004@fiu.edu>
2024-10-12 00:49:44 -0400

    Initial Commit

---
+++ PhotoScavengerHunt/PhotoScavengerHuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoScavengerHuntApp.swift
+//  PhotoScavengerHunt
+//
+//  Created by user on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotoScavengerHuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
